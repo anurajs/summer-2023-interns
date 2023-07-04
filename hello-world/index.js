5ec46f30d5b3aeb3aea6fa585c039d37c8e86af8
Anuraj Tapidas <atapidas@gmail.com>
2023-07-04 17:07:40 +0100

    npm project

---
+++ hello-world/index.js
@@ -0,0 +1,9 @@
+// import secretNumber from './library/index.js';
+// import { v4 } from 'uuid';
+const uuid = require('uuid');
+
+console.log('hello world');
+
+// console.log(secretNumber);
+
+console.log(uuid.v4());
---
+++ hello-world/library/index.js
@@ -0,0 +1,6 @@
+export function isOdd(num) {
+  return num % 2 == 1;
+}
+
+const something = 5;
+export default something;
