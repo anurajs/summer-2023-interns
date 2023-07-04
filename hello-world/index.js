d90bcc0b25e491236fad1fbdc97b8e4d00c009a3
Anuraj Tapidas <atapidas@gmail.com>
2023-07-04 17:29:26 +0100

    fetch example

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,8 +1,17 @@
-import secretNumber from './library/index.js';
+import secretNumber from './lib.js';
 import { v4 } from 'uuid';
+import fetch from 'node-fetch';
 
 console.log('hello world');
 
 console.log(secretNumber);
 
 console.log(v4());
+
+async function fetchThis() {
+  let result = await fetch('https://randomuser.me/api/');
+  result = await result.json();
+  console.log(result.results[0].name.first);
+}
+
+fetchThis();
